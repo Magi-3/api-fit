2885ac8214b5b419571039acef392026242913eb
Enzo Bezerra Moura <mourapastel@gmail.com>
2024-06-18 01:33:11 -0300

    adding bucket

--- src/main/java/me/magi/fitcore/api/controller/page/PageController.java
+++ src/main/java/me/magi/fitcore/api/controller/page/PageController.java
@@ -11,6 +11,6 @@ public class PageController {
         model.addAttribute("mensagem", "Olá, Mundo!");
         model.addAttribute("appName", "Fit Focus");
         model.addAttribute("description", "Baixe o nosso aplicativo de alimentos e aproveite as melhores ofertas e produtos frescos!");
-        model.addAttribute("apkUrl", "/api/v1/download/apk/enzoInfo.ods");        return "homepage";
+        model.addAttribute("apkUrl", "https://storage.cloud.google.com/appreactfit/Captura%20de%20tela%202024-04-24%20201610.png?_ga=2.134160014.-147884152.1718682549");        return "homepage";
     }
 }
